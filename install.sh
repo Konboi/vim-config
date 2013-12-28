6fa05b5a826a4d14a6257864b9c967913c54baf2
Konboi <ryosuke.yabuki@gmail.com>
2013-12-28 15:07:06 +0000

    Added my initialize vim-config

---
+++ install.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/config/vim-config/vim/bundle
+ln -s ~/config/vim-config/vimrc ~/.vimrc
+ln -s ~/config/vim-config/vim ~/.vim
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
